6912762761f9155f90b3491c53235213f417f0c3
iva <ivvvpol@gmail.com>
2022-10-30 17:09:37 +0100

    menu animation

--- script.js
+++ script.js
@@ -1,8 +1,69 @@
-const bgTransition = KUTE.fromTo(
-  '#animation1',
-  { path: '#animation1' },
-  { path: '#animation2' },
-  { repeat: 999, duration: 3000, yoyo: true }
-)
+const menu = document.getElementById('menu')
+const middle = document.getElementById('middle')
+const bottom = document.getElementById('bottom')
+const topSpan = document.getElementById('top')
+const nav = document.getElementById('nav')
+let menuActive = false
+nav.style.visibility = 'hidden'
+nav.style.opacity = '0'
+nav.style.transform = 'translateX(300px)'
 
-bgTransition.start()
+// menu.addEventListener('click', () => {
+//   middle.style.transform = 'translateX(50px)'
+//   middle.style.opacity = '0'
+//   middle.style.transition = "all 1s"
+//   topSpan.style.transform = 'rotate(45deg)'
+//   topSpan.style.transition = "all 1s"
+//   bottom.style.transform = 'rotate(-45deg)'
+//   bottom.style.transition = "all 1s"
+//   nav.style.visibility = 'visible'
+//   nav.style.opacity = '100'
+//   nav.style.transform = 'translateX(0px)'
+//   nav.style.transition = "all 1s"
+//   menu.classList.add('active')
+// })
+
+menu.addEventListener('click', () => {
+  if (menuActive === false) {
+    openMenu()
+  } else if(menuActive === true) {
+    closeMenu()
+  }
+})
+
+function openMenu() {
+  middle.style.transform = 'translateX(50px)'
+  middle.style.opacity = '0'
+  middle.style.transition = "all 1s"
+  topSpan.style.transform = 'rotate(45deg)'
+  topSpan.style.transition = "all 1s"
+  bottom.style.transform = 'rotate(-45deg)'
+  bottom.style.transition = "all 1s"
+  nav.style.visibility = 'visible'
+  nav.style.opacity = '100'
+  nav.style.transform = 'translateX(0px)'
+  nav.style.transition = "all 1s"
+  menuActive = true
+}
+
+function closeMenu() {
+  middle.style.transform = 'translateX(0px)'
+  middle.style.opacity = '100'
+  middle.style.transition = "all 1s"
+  topSpan.style.transform = 'rotate(0deg)'
+  topSpan.style.transform = 'translateY(-10px)'
+  topSpan.style.transition = "all 1s"
+  bottom.style.transform = 'rotate(0deg)'
+  bottom.style.transform = 'translateY(10px)'
+  bottom.style.transition = "all 1s"
+  nav.style.visibility = 'hidden'
+  nav.style.opacity = '0'
+  nav.style.transform = 'translateX(300px)'
+  nav.style.transition = "all 1s"
+  menuActive = false
+}
+
+
+
+
+
